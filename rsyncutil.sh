5927453f3bbdb137ef8c78406006bf35f47bfef3
ryanmcginnis <mcginnisrr@gmail.com>
2015-08-08 13:03:00 -0700

    Update rsyncutil.sh
    Added run-again option.

--- rsyncutil.sh
+++ rsyncutil.sh
@@ -1,7 +1,29 @@
-echo "Type or drag-and-drop source, then Return."
+#!/bin/sh
+
+clear
+rsyncutil() {
+echo "Type or drag and drop source directory, then Return."
 read src
-echo "Type or drag-and-drop destination, then Return.
+clear
+echo "Type or drag and drop destination directory, then Return."
 read des
-sudo rsync -arv $src $des
-echo "Done."
-exit 1
+clear
+echo "Copying ..."
+echo
+echo
+sleep 1
+sudo rsync -arv -h --progress $src $des
+sleep 5 # so user can see results
+echo
+echo
+echo "Done!"
+echo "Run again?"
+read answer
+if [[ $answer ~= ^([yY][eE][sS]|[yY])$ ]]; then
+	rsyncutil()
+else:
+	exit 1
+fi
+}
+
+rsyncutil()
